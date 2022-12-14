98e6de299e057298f5dde93e44822af1ea0556b4
maya koma <mayakoma@gmail.com>
2022-12-14 18:50:47 +0200

    delete

--- app/src/main/java/com/example/studentsapp/model/Model.java
+++ app/src/main/java/com/example/studentsapp/model/Model.java
@@ -3,18 +3,30 @@ package com.example.studentsapp.model;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Model {
+public class Model { //singleton pattern
 
     private static final Model _instance=new Model();
 
     static public Model instance(){return _instance;}
 
     private Model(){
+        for (int i = 0; i < 10; i++) {
+            Student s= new Student();
+            s.setAddress("address");
+            s.setCb(false);
+            s.setId(i+"");
+            s.setPhone("050");
+            s.setAvatarUrl("");
+            s.setName("name " + i);
+            addStudent(s);
+
+        }
 
     }
 
     List<Student> data= new LinkedList<>();
 
+    //have to be public because its inside package and we want to use it outside
     public List<Student> getAllStudents(){
         return data;
     }
